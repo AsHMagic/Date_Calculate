70d67923644cd90045708bb1b19b985575bc2402
AsHMagic <84615219+AsHMagic@users.noreply.github.com>
2023-02-27 12:51:34 +0900

    Update Date.py

--- Date.py
+++ Date.py
@@ -6,14 +6,14 @@ import datetime
 
 day = datetime.datetime.now()
 
-유무 = str(input('날짜 더하기 혹은 빼기를 써주세요: '))
+scan = str(input('날짜 더하기 혹은 빼기를 써주세요: '))
 
-if 유무 == "더하기":
-    날짜 = int(input('더하실 날짜를 적어주세요(숫자만): '))
-    dayplus = day + datetime.timedelta(days=날짜)
+if scan == "더하기":
+    date = int(input('더하실 날짜를 적어주세요(숫자만): '))
+    dayplus = day + datetime.timedelta(days=date)
     print(dayplus)
 
-elif 유무 == "빼기":
-    날짜 = int(input('빼실 날짜를 적어주세요(숫자만): '))
-    dayminus = day - datetime.timedelta(days=날짜)
+elif scan == "빼기":
+    date = int(input('빼실 날짜를 적어주세요(숫자만): '))
+    dayminus = day - datetime.timedelta(days=date)
     print(dayminus)
